e4af6aecb03026c6d92d1c6fab69f676d3834d02
Thore Bücking <thore@buecking.me>
2020-07-12 15:31:59 +0100

    created command line interface for testing

--- test_if_fake.py
+++ test_if_fake.py
@@ -10,11 +10,10 @@ from keras.models import load_model
 import cv2
 import numpy as np
 import os
+import sys
 
 def test_if_fake(video_file):
     cap = cv2.VideoCapture(video_file)
-    # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     filename,_ = video_file.split('.')
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
       #Load Neural Net
@@ -66,3 +65,10 @@ def test_if_fake(video_file):
     out_cap.release()
     cv2.destroyAllWindows()
     print('Finished writing to file')
+    
+def main():
+    file = sys.argv[1]  
+    test_if_fake(file)
+    
+if __name__ == '__main__':
+    main()
